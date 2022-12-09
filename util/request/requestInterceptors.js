6ea5b074facb2ac9cb09cd89b6c8ab3b64d86d3a
bloomingg <1293499952@qq.com>
2022-12-09 21:19:36 +0800

    fix: close uniStatistics & api config update

--- util/request/requestInterceptors.js
+++ util/request/requestInterceptors.js
@@ -8,7 +8,7 @@ module.exports = (vm, ) => {
 			// 初始化请求拦截器时，会执行此方法，此时data为undefined，赋予默认{}
 			config.data = config.data || {}
 			if (config.custom.isIMApi) {
-				config.baseURL = appConfig.registerUrl
+				config.baseURL = appConfig.apiUrl
 			}
 			// 可以在此通过vm引用vuex中的变量，具体值在vm.$store.state中
 			return config
