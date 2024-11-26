f23d982e537d9c61e29bd046498486aaef331f01
kevin <1026838160@qq.com>
2024-11-26 10:18:33 +0800

    feat: update config

--- common/config.js
+++ common/config.js
@@ -1,30 +1,21 @@
-// const registerUrl = 'https://xxx.com/chat'
-// const apiUrl = 'https://xxx.com/api'
 // const wsUrl = 'wss://xxx.com/msg_gateway'
+// const apiUrl = 'https://xxx.com/api'
+// const registerUrl = 'https://xxx.com/chat'
 
-const registerUrl = 'http://your-server-ip:10008'
-const configUrl = 'http://your-server-ip:10009'
+const wsUrl = 'ws://your-server-ip:10001'
 const apiUrl = 'http://your-server-ip:10002'
+const registerUrl = 'http://your-server-ip:10008'
 
-let wsUrl = ''
-
-// #ifdef APP-PLUS
-wsUrl = 'ws://your-server-ip:10001'
-// #endif
-
-// #ifdef H5 || MP-WEIXIN
-wsUrl = 'ws://your-server-ip:10003'
-// #endif
-
-const version = 'OpenCorp-Uni-Demo'
+const version = 'OpenCorp-Uni-Demo 3.8.2'
 
 // 高德地图web api key  用于根据经纬度生成快照  当前key已绑定安卓包名  需要自行申请替换
 const AmapWebKey = "";
 
 const getRegisterUrl = () => uni.getStorageSync("IMRegisteUrl") || registerUrl;
-const getConfigUrl = () => uni.getStorageSync("IMConfigUrl") || configUrl;
 const getApiUrl = () => uni.getStorageSync("IMApiUrl") || apiUrl;
 const getWsUrl = () => uni.getStorageSync("IMWsUrl") || wsUrl;
+
+const getConfigUrl = () => '';
 
 module.exports = {
   version,
